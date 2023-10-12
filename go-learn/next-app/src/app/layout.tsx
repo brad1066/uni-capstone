786e535bbe938ea6adf46d5d22ac74a37ca4be1c
Bradley Hastings <bradleybeasley2000@gmail.com>
2023-10-12 15:03:07 +0100

    Addition to previous commit

--- go-learn/next-app/src/app/layout.tsx
+++ go-learn/next-app/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -15,8 +16,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+        <body className={inter.className}>{children}</body>
+      </ThemeProvider>
     </html>
   )
 }
