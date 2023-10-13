55eb5d8c28d1eb8273e6e5d9bb7fce0ed02dd766
Bradley Hastings <bradleybeasley2000@gmail.com>
2023-10-13 15:49:05 +0100

    update to structure

--- go-learn/next-app/src/dummy-data/students.ts
+++ go-learn/next-app/src/dummy-data/students.ts
@@ -2,22 +2,24 @@ import { TStudent } from "@/lib/types";
 import { tj000001, tj000002 } from "./users";
 import { teacher1, teacher2 } from "./teachers";
 import { class1, class2 } from "./classes";
+import { course1, course2 } from "./courses";
 
 export const student1: TStudent = {
     id: '0000-0000-0000-0001',
     user: tj000001,
+    enrolledCourse: course1,
+    enrolledClasses: [class1],
+    personalTutor: teacher1,
     emergencyContact: {
         label: 'Mum',
         email: 'test@test.com'
     },
-    enrolledClasses: [class1],
     homeAddress: {
         addressLine1: '1 Homey Home',
         stateCounty: 'county',
         town: 'town',
         zipPostCode: 'AB1 2CD'
     },
-    personalTutor: teacher1,
     termAddress: {
         addressLine1: '1 Homey Home',
         stateCounty: 'county',
@@ -29,11 +31,12 @@ export const student1: TStudent = {
 export const student2: TStudent = {
     id: '0000-0000-0000-0002',
     user: tj000002,
+    enrolledCourse: course2,
+    enrolledClasses: [class2],
     emergencyContact: {
         label: 'Mum',
         email: 'test@test.com'
     },
-    enrolledClasses: [class2],
     homeAddress: {
         addressLine1: '1 Homey Home',
         stateCounty: 'county',
--- go-learn/next-app/src/lib/types.ts
+++ go-learn/next-app/src/lib/types.ts
@@ -1,92 +1,91 @@
 export type TUser = {
-    username: string
-    password: string
+    username?: string
     title?: string
     forename?: string
     middleNames?: string
     surname?: string
     letters?: string
     contactDetails?: TContact
-    role: 'admin' | 'teacher' | 'student' | 'unassigned'
+    role?: 'admin' | 'teacher' | 'student' | 'unassigned'
 }
 
 export type TStudent = {
-    id: string
-    user: TUser 
-    emergencyContact: TContact
-    termAddress: TAddress
-    homeAddress: TAddress
-    personalTutor: TTeacher 
-    enrolledClasses: TClass[]
+    id?: string
+    user?: TUser 
+    emergencyContact?: TContact
+    termAddress?: TAddress
+    homeAddress?: TAddress
+    personalTutor?: TTeacher 
+    enrolledClasses?: TClass[]
     enrolledCourse?: TCourse 
 }
 
 export type TTeacher = {
-    id: string
-    user: TUser 
-    address: TAddress
-    students: TStudent[]
-    classes: TClass[]
+    id?: string
+    user?: TUser 
+    address?: TAddress
+    students?: TStudent[]
+    classes?: TClass[]
 }
 
 export type TClass = {
-    id: string
-    title: string
-    students: TStudent[]
-    teachers: TTeacher[]
-    module: TModule
+    id?: string
+    title?: string
+    students?: TStudent[]
+    teachers?: TTeacher[]
+    module?: TModule
 }
 
 export type TCourse = {
-    id: string
-    title: string
-    description: string
-    websiteURL: string
-    students: TStudent[]
-    modules: TModule[]
+    id?: string
+    title?: string
+    description?: string
+    websiteURL?: string
+    students?: TStudent[]
+    modules?: TModule[]
 }
 
 export type TModule = {
-    id: string
-    title: string
-    description: string
-    websiteURL: string
-    classes: TClass[]
-    units: TUnit[]
-    courses: TCourse[]
+    id?: string
+    title?: string
+    description?: string
+    websiteURL?: string
+    classes?: TClass[]
+    units?: TUnit[]
+    courses?: TCourse[]
 }
 
 export type TUnit = {
-    id: string
-    title: string
-    description: string
-    sections: TSection[]
-    resources: TResource[]
-    module: TModule
+    id?: string
+    title?: string
+    description?: string
+    sections?: TSection[]
+    resources?: TResource[]
+    module?: TModule
 }
 
 export type TSection = {
-    id: string
-    title: string
-    description: string
-    resources: TResource[]
-    unit: TUnit
+    id?: string
+    title?: string
+    description?: string
+    resources?: TResource[]
+    unit?: TUnit
 }
 
 export type TResource = {
-    id: string
-    title: string
-    description: string
-    content: string
-    units: TUnit[]
-    sections: TSection[]
+    id?: string
+    title?: string
+    description?: string
+    content?: string
+    units?: TUnit[]
+    sections?: TSection[]
 }
 
 export type TAddress = {
-    addressLine1: string
+    addressLine1?: string
     addressLine2?: string
-    town: string
-    stateCounty: string
+    town?: string
+    stateCounty?: string
     zipPostCode?: string
 }
 
