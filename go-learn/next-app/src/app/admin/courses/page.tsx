78dbae2cc5690cd43bc76dd3106c8712a329f3d0
Bradley Hastings <bradleybeasley2000@gmail.com>
2023-10-19 18:39:49 +0100

    Basic implementation of Courses list page

--- go-learn/next-app/src/app/admin/courses/page.tsx
+++ go-learn/next-app/src/app/admin/courses/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 
+import AdminCourseItem from "@/components/admin/AdminCourseItem";
+import courses from "@/dummy-data/courses";
 import { useAuth } from "@/hooks/useAuth";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -18,6 +21,17 @@ export default function CoursesAdminPage() {
   }, [])
   return (
     <>
+      {(!loading && user?.role != 'admin') && <>
+        Sorry, but you cannot access this resource. <Link href='/'>Go Home</Link>
+      </>}
+      {!loading && user?.role == 'admin' && <>
+        <h1 className="mb-[1rem]">Courses</h1>
+        {courses?.length > 0 && <div className="grid grid-cols-3 w-full gap-5">
+          {courses.map(course => (
+            <AdminCourseItem course={course} key={course.id} onDelete={async () => {}}/>
+          ))}
+        </div>}
+      </>}
     </>
   )
 }
