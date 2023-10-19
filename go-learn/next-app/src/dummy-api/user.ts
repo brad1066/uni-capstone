bd22853e9eba8065c3fc78010ec9a4a38e757b9e
Bradley Hastings <bradleybeasley2000@gmail.com>
2023-10-19 17:32:19 +0100

    Added dummy newUser API function

--- go-learn/next-app/src/dummy-api/user.ts
+++ go-learn/next-app/src/dummy-api/user.ts
@@ -1,7 +1,10 @@
 import { TUser } from "@/lib/types";
 
-export const newUser = (user: TUser) => {
-    user.username = `${user.forename?.charAt(0).toLowerCase()}${user.surname?.charAt(0).toLowerCase()}${Math.floor(Math.random()*100000)}`
+export const newUser = async (user: TUser) => {
+    if (user.role == 'student')
+        user.username = `${user.forename?.charAt(0).toLowerCase()}${user.surname?.charAt(0).toLowerCase()}${Math.floor(Math.random() * 100000)}`
+    else if (user?.role == 'teacher')
+        user.username = `${user.forename?.charAt(0).toLowerCase()}${user.surname?.toLowerCase()}1`
 
     return user
 }
