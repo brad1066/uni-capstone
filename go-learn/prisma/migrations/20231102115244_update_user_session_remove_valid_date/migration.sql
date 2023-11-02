f500f1fd2b507db118ed4726259db1732553175e
Bradley Hastings <bradleybeasley2000@gmail.com>
2023-11-02 11:53:57 +0000

    added UserSession to db schema

---
+++ go-learn/prisma/migrations/20231102115244_update_user_session_remove_valid_date/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `validUntil` on the `UserSession` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserSession" DROP COLUMN "validUntil";
