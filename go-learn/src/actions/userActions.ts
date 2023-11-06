bca84422f8c767a478d92829fee09c6c24a98950
Bradley Hastings <bradleybeasley2000@gmail.com>
2023-11-06 14:31:28 +0000

    update userAction typings

--- go-learn/src/actions/userActions.ts
+++ go-learn/src/actions/userActions.ts
@@ -1,121 +1,113 @@
 'use server'
 
 import prisma from "@/lib/db"
-import { TStudent, TTeacher, TUser } from "@/lib/types"
+import { TUser } from "@/lib/types"
+import { genRandomPassword } from "@/lib/utils"
 import { User, UserRole } from "@prisma/client"
 import bcrypt from "bcrypt"
 import { cookies } from "next/headers"
 import { env } from "process"
 
-export async function CreateUser(userInfo: TUser): Promise<TUser> {
-    const rNum = Math.floor((Math.random()) * 100000)
-    let user = await prisma.user.create({
-        data: {
-            username: `${userInfo.forename?.toLowerCase().charAt(0)}${userInfo.surname?.toLowerCase().charAt(0)}${rNum}`,
-            title: userInfo?.title || 'Mr',
-            forename: userInfo.forename || '',
-            surname: userInfo.surname || '',
-            middleNames: userInfo.middleNames || '',
-            letters: userInfo.letters || '',
-            role: userInfo?.role || 'unassigned',
+/**
+ * Creates a user with a default password
+ * @param userInfo A TUser object to create a new user from
+ * @returns Promise of a User object (as defined by Prisma)
+ */
+export async function createUser(userInfo: TUser): Promise<User> {
+  const rNum = Math.floor((Math.random()) * 100000)
+  let user = await prisma.user.create({
+    data: {
+      username: `${userInfo.forename?.toLowerCase().charAt(0)}${userInfo.surname?.toLowerCase().charAt(0)}${rNum}`,
+      title: userInfo?.title || 'Mr',
+      forename: userInfo.forename || '',
+      surname: userInfo.surname || '',
+      middleNames: userInfo.middleNames || '',
+      letters: userInfo.letters || '',
+      role: userInfo?.role || 'unassigned',
       password: bcrypt.hashSync(userInfo.password || genRandomPassword(), env.PASSWORD_HASH as string)
-        }
+    }
+  })
+  if (userInfo.contactDetails?.email || userInfo.contactDetails?.mobile) {
+    const contact = await prisma.contact.create({
+      data: {
+        label: 'primary',
+        email: userInfo?.contactDetails?.email || '',
+        mobile: userInfo?.contactDetails?.mobile || '',
+      }
     })
-    if (userInfo.contactDetails?.email || userInfo.contactDetails?.mobile) {
-        const contact = await prisma.contact.create({
-            data: {
-                label: 'primary',
-                email: userInfo?.contactDetails?.email || '',
-                mobile: userInfo?.contactDetails?.mobile || '',
-            }
-        })
 
-        user = await prisma.user.update({
-            where: { username: user.username },
-            data: { contactId: contact.id },
-        })
-    }
+    user = await prisma.user.update({
+      where: { username: user.username },
+      data: { contactId: contact.id },
+    })
+  }
 
-    if (user.role == 'teacher') {
-        await prisma.teacher.create({
-            data: { username: user.username },
-            select: { user: true }
-        }).then(teacher => { user = teacher.user })
-    }
+  if (user.role == 'teacher') {
+    await prisma.teacher.create({
+      data: { username: user.username },
+      select: { user: true }
+    }).then(teacher => { user = teacher.user })
+  }
 
-    if (user.role == 'student') {
-        await prisma.student.create({
-            data: { username: user.username },
-            select: { user: true }
-        }).then(student => { user = student.user })
-    }
+  if (user.role == 'student') {
+    await prisma.student.create({
+      data: { username: user.username },
+      select: { user: true }
+    }).then(student => { user = student.user })
+  }
 
-    return user as TUser
+  return user
 }
 
+// GET actions for User[]
 export async function getUsers(roles: UserRole[] = []): Promise<TUser[]> {
-    const authCookie = cookies().get('auth')
-    if (authCookie) {
-        const [session, users] = await prisma.$transaction([
-            prisma.userSession.findFirst({ where: { cookieValue: authCookie.value }, select: { user: true } }),
-            prisma.user.findMany()
-        ])
-        if (roles.length == 0 || roles.includes(session?.user.role as UserRole))
-            return users as TUser[]
-    }
-    return []
-}
-
-export async function getUser(username: string, roles: UserRole[] = []): Promise<TUser | undefined> {
-    const authCookie = cookies().get('auth')
-    if (authCookie) {
-        const [session, user] = await prisma.$transaction([
-            prisma.userSession.findFirst({ where: { cookieValue: authCookie.value }, select: { user: true } }),
-            prisma.user.findUnique({ where: { username } })
-        ])
-        if (roles.length == 0 || roles.includes(session?.user.role as UserRole))
-            return user as TUser
-    }
-    return undefined
-}
-
-export async function changePassword(username: string, password: string): Promise<TUser | undefined> {
-    const authCookie = cookies().get('auth')
-    if (authCookie) {
-        const [session, user] = await prisma.$transaction([
-            prisma.userSession.findFirst({ where: { cookieValue: authCookie.value }, select: { user: true } }),
-            prisma.user.update({
-                where: { username },
-                data: {
-                    password: bcrypt.hashSync(password, env.PASSWORD_HASH as string)
-                }
-            })])
-        return user as TUser
-    } else return undefined
+  const authCookie = cookies().get('auth')
+  if (authCookie) {
+    const [session, users] = await prisma.$transaction([
+      prisma.userSession.findFirst({ where: { cookieValue: authCookie.value }, select: { user: true } }),
+      prisma.user.findMany()
+    ])
+    if (roles.length == 0 || roles.includes(session?.user.role as UserRole))
+      return users as TUser[]
+  }
+  return []
 }
 
 export async function getUsersByRole(roles: UserRole[] = []) {
-    const authCookie = cookies().get('auth')
-    if (authCookie) {
-        const [session, admin, teachers, students] = await prisma.$transaction([
-            prisma.userSession.findFirst({ where: { cookieValue: authCookie.value }, select: { user: true } }),
-            prisma.user.findMany({
-                where: { role: 'admin' },
-                take: 5
-            }),
-            prisma.user.findMany({
-                where: { role: 'teacher' },
-                take: 5
-            }),
-            prisma.user.findMany({
-                where: { role: 'student' },
-                take: 5
-            })
-        ])
-        if (session?.user.role == 'admin') {
-            return [...admin, ...teachers, ...students]
-        }
-    } else return undefined
+  const authCookie = cookies().get('auth')
+  if (authCookie) {
+    const [session, admin, teachers, students] = await prisma.$transaction([
+      prisma.userSession.findFirst({ where: { cookieValue: authCookie.value }, select: { user: true } }),
+      prisma.user.findMany({
+        where: { role: 'admin' },
+        take: 5
+      }),
+      prisma.user.findMany({
+        where: { role: 'teacher' },
+        take: 5
+      }),
+      prisma.user.findMany({
+        where: { role: 'student' },
+        take: 5
+      })
+    ])
+    if (session?.user.role == 'admin') {
+      return [...admin, ...teachers, ...students]
+    }
+  } else return undefined
+}
+
+export async function getUser(username: string, roles: UserRole[] = []): Promise<User | undefined> {
+  const authCookie = cookies().get('auth')
+  if (authCookie) {
+    const [session, user] = await prisma.$transaction([
+      prisma.userSession.findFirst({ where: { cookieValue: authCookie.value }, select: { user: true } }),
+      prisma.user.findUnique({ where: { username } })
+    ])
+    if (roles.length == 0 || roles.includes(session?.user.role as UserRole))
+      return user as User
+  }
+  return undefined
 }
 
 /**
@@ -125,16 +117,15 @@ export async function getUsersByRole(roles: UserRole[] = []) {
  * @returns TUser if user is found, otherwise null
  */
 export async function checkLoginCredentials(username: string, password: string): Promise<TUser | null> {
-    password = await bcrypt.hash(password, env.PASSWORD_HASH as string)
-    const user = await prisma.user
-        .findFirst({ where: { 'username': username, password } })
+  password = await bcrypt.hash(password, env.PASSWORD_HASH as string)
+  const user = await prisma.user
+    .findFirst({ where: { 'username': username, password } })
 
-    return user as TUser || null
+  return user as TUser || null
 
 }
 
 export async function deleteUser(user: User) {
-    const resp = await prisma.user.delete({ where: { username: user.username } })
-    console.log(resp)
-    return resp
+  const resp = await prisma.user.delete({ where: { username: user.username } })
+  return resp
 }
