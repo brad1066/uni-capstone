54e34100dbc86f689100436d62b19457f6bca1f5
Bradley Hastings <bradleybeasley2000@gmail.com>
2023-11-13 13:47:44 +0000

    Updated profile/[username] page

--- go-learn/src/app/profile/[username]/page.tsx
+++ go-learn/src/app/profile/[username]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { updateUser } from "@/actions/userActions"
+import { getUser, updateUser } from "@/actions/userActions"
 import UserEditForm from "@/components/forms/UserEditForm"
 import { useToast } from "@/components/ui/use-toast"
 import { useAuth } from "@/hooks/useAuth"
@@ -9,7 +9,13 @@ import { ToastAction } from "@radix-ui/react-toast"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
-const ProfilePage = () => {
+type ProfilePageProps = {
+  params: {
+    username: string
+  }
+}
+
+const ProfilePage = ({ params: { username } }: ProfilePageProps) => {
 
   const { toast } = useToast()
 
@@ -20,7 +26,7 @@ const ProfilePage = () => {
   const [user, setUser] = useState(signedInUser)
   const [updated, setUpdated] = useState(false)
 
-  const userUpdateSuccess = () => toast({ title: "Updated", description: "Your details have been updated"})
+  const userUpdateSuccess = () => toast({ title: "Updated", description: "Your details have been updated" })
   const userUpdateFailed = () => toast({
     title: "Not updated",
     description: "There was an issue updating your account details",
@@ -33,22 +39,19 @@ const ProfilePage = () => {
       if (!signedInUser) await validateLoggedIn?.().then(({ loggedIn }) => {
         if (!loggedIn) router.replace('/login')
       })
+      setUser(await getUser(username))
       setLoading(false)
     })()
   }, [])
-
-  useEffect(() => {
-    setUser(signedInUser)
-  }, [signedInUser])
 
   useEffect(() => {
     if (!updated && !loading && user != signedInUser) setUpdated(true)
   }, [user])
 
   return <>
-    <h1 className="mb-[2rem]">Hi {signedInUser?.forename} {signedInUser?.surname}</h1>
+    <h1 className="mb-[2rem]">Hi {user?.forename} {user?.surname}</h1>
     <div className="flex gap-[2rem] w-full flex-col xl:flex-row">
-      <UserEditForm user={user as User} setUser={setUser} onUpdateSave={(user) => {
+      <UserEditForm user={user as User} setUser={setUser} canEdit={user?.username == signedInUser?.username || signedInUser?.role == 'admin'} onUpdateSave={(user) => {
         if (updated) updateUser(user).then(validateLoggedIn).then(userUpdateSuccess).then(() => { setUpdated(false) }).catch(userUpdateFailed)
       }} />
     </div>
--- go-learn/src/components/forms/UserEditForm.tsx
+++ go-learn/src/components/forms/UserEditForm.tsx
@@ -16,10 +16,11 @@ type UserEditFormProps = {
   loading?: boolean
   user: User
   setUser: Dispatch<SetStateAction<User | undefined>>
+  canEdit?: boolean
   onUpdateSave?: (user: User) => void
 }
 
-const UserEditForm = ({ loading, user, setUser, onUpdateSave }: UserEditFormProps) => {
+const UserEditForm = ({ loading, user, setUser, onUpdateSave, canEdit }: UserEditFormProps) => {
   const [passwordDialogOpen, setPasswordDialogOpen] = useState<boolean>(false)
   const [passwordDialogDisabled, setPasswordDialogDisabled] = useState<boolean>(false)
 
@@ -31,13 +32,13 @@ const UserEditForm = ({ loading, user, setUser, onUpdateSave }: UserEditFormProp
         <CardHeader>
           <CardTitle className="flex space-between items-center">
             User data
-            <Button variant="default" className="ml-auto" onClick={() => onUpdateSave?.(user)}>Update</Button>
+            {canEdit && <Button variant="default" className="ml-auto" onClick={() => onUpdateSave?.(user)}>Update</Button>}
           </CardTitle>
         </CardHeader>
         <CardContent>
           <ul>
             <li className="hover:bg-accent">Username <span>{user?.username}</span></li>
-            <li className="hover:bg-accent">Password
+            {canEdit && <li className="hover:bg-accent">Password
               <Dialog open={passwordDialogOpen} onOpenChange={setPasswordDialogOpen}>
                 <DialogTrigger asChild><Button>Change Password <Pencil2Icon className="ml-1" /></Button></DialogTrigger>
                 <DialogContent>
@@ -51,7 +52,7 @@ const UserEditForm = ({ loading, user, setUser, onUpdateSave }: UserEditFormProp
                     setPasswordDialogOpen(false)
                   }} />
                 </DialogContent>
-              </Dialog></li>
+              </Dialog></li>}
             <li className="flex-col items-start hover:bg-accent">
               Name
               <Collapsible
@@ -61,14 +62,14 @@ const UserEditForm = ({ loading, user, setUser, onUpdateSave }: UserEditFormProp
               >
                 <div className="alwaysShown">
                   {/* Name's 'title' input */}
-                  <Input placeholder="title" value={user?.title} onChange={({ target: { value: title } }) => (setUser(user => user ? { ...user, title } : user
+                  <Input disabled={!canEdit} placeholder="title" value={user?.title} onChange={({ target: { value: title } }) => (setUser(user => user ? { ...user, title } : user
                   ))} />
 
                   {/* Name's 'forename' input */}
-                  <Input placeholder="forename" value={user?.forename} onChange={({ target: { value: forename } }) => (setUser(user => user ? { ...user, forename } : user))} />
+                  <Input disabled={!canEdit} placeholder="forename" value={user?.forename} onChange={({ target: { value: forename } }) => (setUser(user => user ? { ...user, forename } : user))} />
 
                   {/* Name's 'surname' input */}
-                  <Input placeholder="surname" value={user?.surname} onChange={({ target: { value: surname } }) => (setUser(user => user ? { ...user, surname } : user))} />
+                  <Input disabled={!canEdit} placeholder="surname" value={user?.surname} onChange={({ target: { value: surname } }) => (setUser(user => user ? { ...user, surname } : user))} />
 
                   {/* The trigger to show/hide the extra fields */}
                   <CollapsibleTrigger asChild>
@@ -81,12 +82,12 @@ const UserEditForm = ({ loading, user, setUser, onUpdateSave }: UserEditFormProp
                 <CollapsibleContent className="grid grid-cols-2 gap-x-4">
                   {/* Name's 'middleNames' input */}
                   <Label className="flex flex-col gap-4">Middle names
-                    <Input placeholder="middle names" value={user?.middleNames || ''} onChange={({ target: { value: middleNames } }) => (setUser(user => user ? { ...user, middleNames } : user))} />
+                    <Input disabled={!canEdit} placeholder="middle names" value={user?.middleNames || ''} onChange={({ target: { value: middleNames } }) => (setUser(user => user ? { ...user, middleNames } : user))} />
                   </Label>
 
                   {/* Name's 'letters' input */}
                   <Label className="flex flex-col gap-4">Letters
-                    <Input placeholder="letters" value={user?.letters || ''} onChange={({ target: { value: letters } }) => (setUser(user => user ? { ...user, letters } : user))} />
+                    <Input disabled={!canEdit} placeholder="letters" value={user?.letters || ''} onChange={({ target: { value: letters } }) => (setUser(user => user ? { ...user, letters } : user))} />
                   </Label>
                 </CollapsibleContent>
               </Collapsible>
@@ -95,12 +96,12 @@ const UserEditForm = ({ loading, user, setUser, onUpdateSave }: UserEditFormProp
               <div className="flex flex-col sm:flex-row gap-4">
                 {/* Contact's 'email' input */}
                 <Label className="flex-1 flex flex-col gap-4">Email
-                  <Input type="email" placeholder="email" />
+                  <Input disabled={!canEdit} type="email" placeholder="email" />
                 </Label>
 
                 {/* Contact's 'mobile' input */}
                 <Label className="flex-1 flex flex-col gap-4">Mobile
-                  <Input placeholder="mobile" />
+                  <Input disabled={!canEdit} placeholder="mobile" />
                 </Label>
               </div>
             </li>
@@ -108,8 +109,8 @@ const UserEditForm = ({ loading, user, setUser, onUpdateSave }: UserEditFormProp
         </CardContent>
       </Card>
     )}
-    
-    </>
+
+  </>
 }
 
 export default UserEditForm
