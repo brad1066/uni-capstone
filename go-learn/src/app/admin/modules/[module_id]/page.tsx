4aec8061c73a9bcf1bc6c9995cce0d74fe3eb39c
Bradley Beasley <bb20066@essex.ac.uk>
2023-11-22 16:20:40 +0000

    Fixed admin/modules/{id} router issue

--- go-learn/src/app/admin/modules/[module_id]/page.tsx
+++ go-learn/src/app/admin/modules/[module_id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useAuth } from "@/hooks/useAuth"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 export default function SingleModuleAdminPage() {
