74c77fbd9ec189b2b16491fc8d25a7d89ae19e7b
Bradley Beasley <bb20066@essex.ac.uk>
2023-12-07 19:35:32 +0000

    Update to Resource schema

---
+++ go-learn/prisma/migrations/20231207192800_update_resource_include_uploads_array/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Resource" ADD COLUMN     "uploads" TEXT[];
--- go-learn/src/actions/resourceActions.ts
+++ go-learn/src/actions/resourceActions.ts
@@ -4,7 +4,7 @@ import { UserRole } from "@prisma/client";
 import { getCurrentUserSession } from "./authActions";
 import prisma from "@/lib/db";
 
-export async function  getResources(username: string = '', roles: UserRole[] = []) {
+export async function getResources(username: string = '', roles: UserRole[] = []) {
   const session = await getCurrentUserSession()
   if (!session?.user || (roles.length > 0 && (session.user.role in roles))) return null
 
