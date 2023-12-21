c713f7e67e0fe19ddc60f382de54ed9ce2529366
Bradley Beasley <bb20066@essex.ac.uk>
2023-12-21 12:39:30 +0000

    Made skeleton for single resource page

---
+++ go-learn/src/app/manage/resources/[resource_id]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+type SingleResourceAdminPageProps = {
+  params: {
+    resource_id: string
+  }
+}
+
+export default function SingleResourceAdminPage({ params: { resource_id } }: SingleResourceAdminPageProps) {
+  return (<>
+
+  </>)
+}
