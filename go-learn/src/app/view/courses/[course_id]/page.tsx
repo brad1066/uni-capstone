fc9ffe392d98f0e7d8b3a811fd4db3ed8b5262b9
Bradley Beasley <bb20066@essex.ac.uk>
2024-01-18 12:14:56 +0000

    Updated single view item for module

--- go-learn/src/app/view/courses/[course_id]/page.tsx
+++ go-learn/src/app/view/courses/[course_id]/page.tsx
@@ -3,8 +3,11 @@
 import { getCourse } from '@/actions/courseActions'
 import NotFoundPage from '@/app/not-found'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Tooltip } from '@/components/ui/tooltip'
 import { useAuth } from '@/hooks/useAuth'
 import { Course, Module } from '@prisma/client'
+import { TooltipContent, TooltipTrigger } from '@radix-ui/react-tooltip'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
@@ -37,7 +40,7 @@ export default function ViewCoursePage({ params: { course_id } }: ViewCoursePage
       </>}
       {!loading && course && <>
         <h1 className="mb-4">{course?.title}</h1>
-        <div className="w-full grid md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div className="w-full flex flex-col gap-4">
           {/* Description Card */}
           <Card className="">
             <CardHeader><CardTitle>Description</CardTitle></CardHeader>
@@ -48,11 +51,22 @@ export default function ViewCoursePage({ params: { course_id } }: ViewCoursePage
           </Card>
 
           {/* Course Modules Card */}
-          <Card className="xl:col-span-2">
+          <Card>
             <CardHeader><CardTitle>Course Modules</CardTitle></CardHeader>
             <CardContent>
-              <ul className="">
-                {course?.modules?.map(module => <li key={module.id}>{module.title}</li>)}
+              <ul className="md:grid md:grid-cols-2 xl:grid-cols-3">
+                {course?.modules?.map(module => (
+                  <Tooltip key={module.id} >
+                    <TooltipTrigger asChild>
+                      <Link
+                        className="border-solid border-primary border-2 rounded-md p-4"
+                        href={`/view/modules/${module.id}`}>{module.title}</Link>
+                    </TooltipTrigger>
+                    <TooltipContent side="top" align="center" className="bg-primary text-white p-2 rounded-md">
+                      Go to {module.title}
+                    </TooltipContent>
+                  </Tooltip>
+                ))}
               </ul>
             </CardContent>
           </Card>
