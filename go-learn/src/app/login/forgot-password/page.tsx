cce49d93077a0f7b2e640ddf7614bca8bfa20e0b
Bradley Hastings <bradleybeasley2000@gmail.com>
2024-04-24 13:16:07 +0100

    Improve forgot password page

--- go-learn/src/app/login/forgot-password/page.tsx
+++ go-learn/src/app/login/forgot-password/page.tsx
@@ -1,15 +1,22 @@
 'use client'
 
 import { requestUserPasswordChange } from '@/actions/authActions'
+import { AlertDialog, AlertDialogAction, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 export default function ForgotPasswordPage() {
-
+  const router = useRouter()
   const [username, setUsername] = useState<string>('')
+  const [alert, setAlert] = useState({
+    open: false,
+    title: '',
+    message: ''
+  })
 
-  return (
+  return (<>
     <div>
       <h1>Forgot Password</h1>
 
@@ -18,10 +25,32 @@ export default function ForgotPasswordPage() {
       <form onSubmit={(e) => {
         e.preventDefault()
         requestUserPasswordChange(username)
+          .then((resp) => {
+            if (resp && resp.success) router.push('/login')
+            else setAlert({
+              open: true,
+              title: 'error',
+              message: 'There was an issue sending the password reset email. Please try again or seek admin assistance.'
+            })
+          })
       }}>
         <Input placeholder="username" value={username} onChange={(e) => { setUsername(e.target.value) }} />
         <Button type="submit">Submit</Button>
       </form>
-    </div>
-  )
+      <AlertDialog open={alert.open}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            {alert?.title && <AlertDialogTitle>{alert.title}</AlertDialogTitle>}
+            {alert?.message && <AlertDialogDescription>
+              {alert.message}
+            </AlertDialogDescription>
+            }
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction onClick={() => { setAlert(alert => ({ ...alert, open: false })) }}>Okay</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div >
+  </>)
 }
