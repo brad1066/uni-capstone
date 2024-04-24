8829d2394c122b7a85ee884c770b77a8bc87192c
Bradley Hastings <bradleybeasley2000@gmail.com>
2024-04-24 13:15:24 +0100

    Improve admin dashboard UX

--- go-learn/src/app/(dashboard)/AdminDashboard.tsx
+++ go-learn/src/app/(dashboard)/AdminDashboard.tsx
@@ -1,11 +1,17 @@
-import { Button } from '@/components/ui/button'
+import { buttonVariants } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import Link from 'next/link'
 
 const AdminDashboard = () => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
-      <Button>Users</Button>
-      <Button>Courses</Button>
+      <Card className='mt-8'>
+        <CardContent className='p-8 flex flex-col gap-4'>
+          <Link href='manage/users' className={buttonVariants({variant: 'default'})}>Users</Link>
+          <Link href='manage/courses' className={buttonVariants({variant: 'default'})}>Courses</Link>
+        </CardContent>
+      </Card>
     </div>
   )
 }
